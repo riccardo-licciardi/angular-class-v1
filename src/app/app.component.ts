9f496c242560dc907402336536bf3eb7cb97a7cb
Riccardo Licciardi <riccardo.licciardi@gmail.com>
2021-07-24 22:14:03 +0200

    Angula wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-class-v1';
+  title = 'Stretta la soglia, larga la via';
 }
